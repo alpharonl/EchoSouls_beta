d8ba1fece5444f8a7e813ee0eb3a7bb2d94ea16e
hentZ1 <zhtyz12@gmail.com>
2025-09-02 18:27:40 -0300

    correçao de bug de movimento

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -11,28 +11,33 @@ public partial class Player : CharacterBody2D
 	}
 	public override void _PhysicsProcess(double delta)
 	{
+		bool mov = false;
 		Godot.Vector2 pos = Position;
 		if (Godot.Input.IsKeyPressed(Key.W))
 		{
 			pos.Y -= speed * (float)delta;
 			anim.Play("Tras");
+			mov = true;
 		}
 		if (Godot.Input.IsKeyPressed(Key.S))
 		{
 			pos.Y += speed * (float)delta;
 			anim.Play("Frente");
+			mov = true;
 		}
 		if (Godot.Input.IsKeyPressed(Key.A))
 		{
 			pos.X -= speed * (float)delta;
 			anim.Play("Esquerda");
+			mov = true;
 		}
 		if (Godot.Input.IsKeyPressed(Key.D))
 		{
 			pos.X += speed * (float)delta;
 			anim.Play("Direita");
+			mov = true;
 		}
-		else
+		if(!mov)
 		{
 			anim.Play("Parado");
 		}

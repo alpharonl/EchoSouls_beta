8c545363a4aa9e457676c2219a6b2479452b08df
hentZ1 <zhtyz12@gmail.com>
2025-09-05 19:52:10 -0300

    adicionado o sistema de hitbox do inimigo

--- Scripts/Clown.cs
+++ Scripts/Clown.cs
@@ -3,16 +3,16 @@ using System;
 
 public partial class Clown : CharacterBody2D
 {
-    public override void _Ready()
-    {
-        Area2D attackArea = GetNode<Area2D>("AttackArea");
-        attackArea.AreaEntered += OnAttackAreaEntered;
-    }
-    private void OnAttackAreaEntered(Area2D area)
-    {
-        if (area.Owner is Player player)
-        {
-            player.TakeDamage(10);
-        }
-    }
+	public override void _Ready()
+	{
+		Area2D attackArea = GetNode<Area2D>("AttackArea");
+		attackArea.AreaEntered += OnAttackAreaEntered;
+	}
+	private void OnAttackAreaEntered(Area2D area)
+	{
+		if (area.Owner is Player player)
+		{
+			player.TakeDamage(10);
+		}
+	}
 }
